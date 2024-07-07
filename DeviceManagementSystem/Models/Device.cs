236a51d5de3447227250bc69c1f36c3cff498c70
undefinedCudaCore <teklithuania@gmail.com>
2024-07-07 10:26:10 +0300

    Add project files.

---
+++ DeviceManagementSystem/Models/Device.cs
@@ -0,0 +1,11 @@
+﻿namespace DeviceManagementSystem.Models
+{
+    internal class Device
+    {
+        internal int DeviceId { get; set; }
+        public string Model { get; set; }
+        public string Manufacturer { get; set; }
+        public DateTime ManufacturDate { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ DeviceManagementSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DeviceManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
